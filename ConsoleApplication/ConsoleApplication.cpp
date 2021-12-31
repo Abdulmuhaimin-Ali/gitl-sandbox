26d2feeeae093255831d71bf31de3361134cd117
Abdul_Ali115 <abdulali115h@gmail.com>
2021-12-30 19:02:31 -0500

    Trying the for loop feature

--- ConsoleApplication/ConsoleApplication.cpp
+++ ConsoleApplication/ConsoleApplication.cpp
@@ -7,6 +7,9 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "This is my first Git Commit!" << std::endl; 
+    std::cout << "This is my first Git Commit!" << std::endl;
+
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
